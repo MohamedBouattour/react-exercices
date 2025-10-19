58aed57cd2697a508804a92f4e56dc4c78caacc8
MohamedBouattour <mohamedbouattour123@gmail.com>
2025-10-19 17:29:07 +0100

    add card style

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,8 +1,17 @@
+const cardStyle = {
+    border: 'solid 2px',
+    padding:'20px',
+    margin:'20px',
+    borderRadius:'15px'
+}
+
 export const ProfileCard = ({user}:any)=>{
-    return(<>
+    return(
+    <div style={cardStyle}>
     <p>Name: {user.name}</p>
     <p>Age: {user.age}</p>
     <p>Job: {user.job}</p>
     <p>Country: {user.country}</p>
-    </>)
+    </div>
+    )
 }
